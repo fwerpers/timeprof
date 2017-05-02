ae64f46ffb45dfc61d263d6669d38f3983004fa1
Felix Werpers <felix@werpers.com>
2017-05-02 12:22:08 +0200

    Change variable names and add docstrings

--- timeprof.py
+++ timeprof.py
@@ -4,6 +4,11 @@ import random
 
 class RandomSchedule:
     def __init__(self, start, end, tags):
+        """Create random schedule from:
+        start -- start time (datetime.datetime)
+        end -- end time (datetime.datetime)
+        tags -- list of tags (list of strings)
+        """
         ntags = len(tags)
         task_list = [Task(start, start+get_random_duration(), random.choice(tags))]
         while (task_list[-1].end < end):
@@ -13,8 +18,14 @@ class RandomSchedule:
             task_list.append(new_task)
 
         self.task_list = task_list
+        self.tags = tags
+        self.duration = end-start
 
     def get_tag(self, time):
+        """Get the tag (string)
+        of a task occupying a certain
+        time (datetime.datetime)
+        """
         i = 0;
         while (task_list[i].start < time):
             i += 1
@@ -22,24 +33,34 @@ class RandomSchedule:
         return(task_list[i].tag)
 
     def get_percentage(self):
+        """Get the time percentage (float)
+        for each tag in the schedule
+        """
         d = {}
-        for tag in tags:
+        for tag in self.tags:
             d[tag] = 0
-        return(dict)
+        for task in self.task_list:
+            d[task.tag] += task.duration.total_seconds()/self.duration.total_seconds()
+        return(d)
 
 def get_random_duration():
+    """Get a duration (datetime.timedelta)
+    from a exponential distribution
+    """
     minutes = np.ceil(20*np.random.exponential(scale=1.3))
     return(datetime.timedelta(0, minutes*60, 0))
 
-class Task(object):
+class Task:
     def __init__(self, start, end, tag):
+        """Create task from:
+        start -- start time (datetime.datetime)
+        end -- end time (datetime.datetime)
+        tag -- (string)
+        """
         self.start = start
         self.end = end
         self.tag = tag
-
-        @property
-        def duration():
-            return(end-start)
+        self.duration = end-start
 
 def main():
     start = datetime.datetime.now()
@@ -49,7 +70,9 @@ def main():
         'mat',
         'sov'
     ]
-    s = RandomSchedule(start, start+duration, tags)
+    schedule = RandomSchedule(start, start+duration, tags)
+    tag_percentage = schedule.get_percentage()
+    print(tag_percentage)
 
 if __name__ == '__main__':
     main()
