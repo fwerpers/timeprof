467970944772f411d1dd13b571e19481e8ff08e8
Felix Werpers <felix@werpers.com>
2017-05-05 15:37:34 +0200

    Change name on main.py

---
+++ experiment.py
@@ -0,0 +1,11 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+def main():
+    #x = 20*np.ceil(np.random.exponential(scale=1.3, size=1000))
+    x = np.ceil(np.random.exponential(scale=20, size=1000))
+    plt.hist(x, bins='auto')
+    plt.show()
+
+if __name__ == '__main__':
+    main()
