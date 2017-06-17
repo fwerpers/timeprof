2de14f62ae6abe50aecfafe87cf46a30a8f701d1
Felix Werpers <felix@werpers.com>
2017-06-17 11:09:19 +0200

    Add code for sampling and data analysis of the simulated data

--- sampletest.py
+++ sampletest.py
@@ -2,14 +2,14 @@ import datetime
 import numpy as np
 from schedule import Activity, RandomSchedule
 import time
+from scipy.special import gammainccinv, gammaincinv
 
 def get_random_interval(mean_interval):
     minutes = np.ceil(np.random.exponential(scale=mean_interval))
     duration = datetime.timedelta(0, minutes*60, 0)
     return(duration)
 
-def generate_ping_times(start, end):
-    mean_interval = 45
+def generate_ping_times(start, end, mean_interval):
     times = [start]
     while True:
         duration = get_random_interval(mean_interval)
@@ -20,10 +20,88 @@ def generate_ping_times(start, end):
 
     return(times)
 
+def wilson_score_interval(tags, tag):
+    N = float(len(tags))
+    n = float(np.sum(tags == tag))
+    # N = float(23)
+    # n = float(13)
+
+    z = 1.96 # 95% confidence interval
+    p = n/N
+    factor = 1/(1+z**2/N)
+    base_term = p + z**2/(2*N)
+    interval_term = z*np.sqrt(p*(1-p)/N+z**2/(4*N**2))
+    low = factor*(base_term - interval_term)
+    high = factor*(base_term + interval_term)
+    res = np.array([low, high])
+    print(res)
+
+def gamma_tom_jack(tags, tag):
+    N = len(tags)
+    n = np.sum(tags == tag)
+    # N = float(23)
+    # n = float(13)
+
+    g = 0.75
+    c = 0.95
+    low = g*gammainccinv(n, (1+c)/2)
+    high = g*gammainccinv(n+1, (1-c)/2)
+    res = np.array([low, high])
+    print(res/N)
+
+def gamma_daniel_reeves(tags, tag):
+    N = len(tags)
+    n = np.sum(tags == tag)
+    # N = float(23)
+    # n = float(13)
+
+    g = 0.75
+    c = 0.95
+    low = g*gammainccinv(n, (1+c)/2)
+    high = g*gammainccinv(n, (1-c)/2)
+    res = np.array([low, high])
+    print(res/N)
+
+def gamma_brute(tags, tag):
+    N = len(tags)
+    n = np.sum(tags == tag)
+    c = 0.95
+    low = gammainccinv(n, (1+c)/2)
+    high = gammainccinv(n, (1-c)/2)
+    res = np.array([low, high])
+    print(res/N)
+
+def gamma_brute2(tags, tag):
+    N = len(tags)
+    n = np.sum(tags == tag)
+    c = 0.95
+    low = gammainccinv(n, (1+c)/2)
+    high = gammainccinv(n+1, (1-c)/2)
+    res = np.array([low, high])
+    print(res/N)
+
+def gamma_brute3(tags, tag):
+    N = len(tags)
+    n = np.sum(tags == tag)
+    c = 0.95
+    low = gammaincinv(n, c/2)
+    high = gammaincinv(n+1, 1-c/2)
+    res = np.array([low, high])
+    print(res/N)
+
+def gamma_wiki(tags, tag):
+    N = len(tags)
+    n = np.sum(tags == tag)
+    c = 0.95
+    low = gammainccinv(n, c/2)
+    high = gammainccinv(n+1, 1-c/2)
+    res = np.array([low, high])
+    print(res/N)
+
 def main():
     """ Example usage """
     start = datetime.datetime.now()
-    duration = datetime.timedelta(1,0,0)
+    duration = datetime.timedelta(2,0,0)
     end = start + duration
     activity_args = [
         ('poop', 15),
@@ -36,11 +114,16 @@ def main():
     tag_percentage = schedule.get_percentage()
     print(tag_percentage)
 
-    ping_times = generate_ping_times(schedule.start, schedule.end)
-
+    ping_times = generate_ping_times(schedule.start, schedule.end, mean_interval=45)
     tags = np.asarray([schedule.get_tag(time) for time in ping_times])
-    print(len(tags))
-    print(tags)
+
+    tag = 'poop'
+    wilson_score_interval(tags, tag)
+    gamma_tom_jack(tags, tag)
+    gamma_daniel_reeves(tags, tag)
+    gamma_brute(tags, tag)
+    gamma_brute2(tags, tag)
+    gamma_brute3(tags, tag)
 
 if __name__ == '__main__':
     main()
