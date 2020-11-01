0dcc0ef7511b0c19e5f3e4cbaac4bb37c29d1959
Felix Verpers <felix@werpers.com>
2020-11-01 13:54:36 +0000

    Add UserClient class

--- data_collection/test.py
+++ data_collection/test.py
@@ -17,29 +17,40 @@ class BotClient(AsyncClient):
     def login(self):
         return super().login(self.mpw)
 
+    async def sync_and_close(self):
+        await self.sync_forever(timeout=SYNC_TIMEOUT)
+        await self.close()
+
+class UserClient(AsyncClient):
+    def __init__(self, homeserver, mid, mpw):
+        super().__init__(homeserver, mid)
+        self.mpw = mpw
+
+    def login(self):
+        return super().login(self.mpw)
+
+    async def sync_and_close(self):
+        await self.sync_forever(timeout=SYNC_TIMEOUT)
+        await self.close()
+
 async def register_dummy_users():
     client = AsyncClient(HOMESERVER_BASE_URL)
     resp = await client.register(BOT_ID, BOT_PW)
     resp = await client.register(USER_ID, USER_PW)
     await client.close()
 
-async def bot_coroutine(client):
-    await client.sync_forever(timeout=SYNC_TIMEOUT)
-    await client.close()
-
 async def user_coroutine():
-    client = AsyncClient(HOMESERVER_BASE_URL, USER_ID)
-    resp = await client.login(USER_PW)
+    user_client = UserClient(HOMESERVER_BASE_URL, USER_ID, USER_PW)
+    resp = await user_client.login()
     logging.info(resp)
-    await client.sync_forever(timeout=SYNC_TIMEOUT)
-    await client.close()
+    await user_client.sync_and_close()
 
 async def main():
     await register_dummy_users()
     bot_client = BotClient(HOMESERVER_BASE_URL, BOT_ID, BOT_PW)
     resp = await bot_client.login()
     logging.info(resp)
-    bot_future = asyncio.run_coroutine_threadsafe(bot_coroutine(bot_client), asyncio.get_event_loop())
+    bot_future = asyncio.run_coroutine_threadsafe(bot_client.sync_and_close(), asyncio.get_event_loop())
     await user_coroutine()
 
 if __name__ == "__main__":
