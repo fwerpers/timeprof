34cdfba020915fdf9e2dc1aeb46256b014358e96
Felix Verpers <felix@werpers.com>
2020-11-03 18:21:58 +0000

    Move user main function into the class

--- data_collection/test.py
+++ data_collection/test.py
@@ -5,6 +5,8 @@ from nio import (
 )
 import logging
 import asyncio
+from datetime import datetime
+import time
 
 SYNC_TIMEOUT = 10000
 HOMESERVER_BASE_URL = "http://localhost:8008"
@@ -13,15 +15,23 @@ BOT_PW = "bot_pw"
 USER_ID = "user_id"
 USER_PW = "user_pw"
 BOT_START_STR = "Hello from bot"
+MSG_TIME_LIMIT_MS = 5e3
+MS_PER_S = 1e3
 
 class BotClient(AsyncClient):
     def __init__(self, homeserver, mid, mpw):
         super().__init__(homeserver, mid)
         self.mpw = mpw
         self.add_event_callback(self.autojoin_room, InviteEvent)
+        self.add_event_callback(self.message_callback, RoomMessageText)
 
     def login(self):
         return super().login(self.mpw)
+
+    async def message_callback(self, room, event):
+        time_now = int(round(time.time() * MS_PER_S))
+        if event.sender != self.user_id and time_now - event.server_timestamp < MSG_TIME_LIMIT_MS:
+            logging.info(event)
 
     async def autojoin_room(self, room, event):
         logging.info(event)
@@ -41,7 +51,9 @@ class BotClient(AsyncClient):
             }
         )
 
-    async def sync_and_close(self):
+    async def main(self):
+        resp = await self.joined_rooms()
+        #logging.info(resp)
         await self.sync_forever(timeout=SYNC_TIMEOUT)
         await self.close()
 
@@ -50,15 +62,18 @@ class UserClient(AsyncClient):
         super().__init__(homeserver, mid)
         self.mpw = mpw
         self.add_event_callback(self.message_callback, RoomMessageText)
+        self.bot_room_id = None
 
     def login(self):
         return super().login(self.mpw)
 
-    def message_callback(self, room, event):
-        logging.info(event)
-        msg = event.body
-        logging.info(msg)
-        self.send_message(room.room_id, "Hello from User")
+    async def message_callback(self, room, event):
+        time_now = int(round(time.time() * MS_PER_S))
+        if event.sender != self.user_id and time_now - event.server_timestamp < MSG_TIME_LIMIT_MS:
+            logging.info(event)
+            msg = event.body
+            logging.info(msg)
+            await self.send_message(room.room_id, "Hello from User")
 
     async def send_message(self, room_id, msg):
         await self.room_send(
@@ -74,44 +89,44 @@ class UserClient(AsyncClient):
         await self.sync_forever(timeout=SYNC_TIMEOUT)
         await self.close()
 
+    async def main(self, bot_user_id):
+        resp = await self.login()
+        logging.info(resp)
+        resp = await self.joined_rooms()
+        #logging.info(resp)
+        joined_rooms = resp.rooms
+        bot_room_exists = False
+        for room in joined_rooms:
+            joined_members = await self.joined_members(room)
+            ms = set([m.display_name for m in joined_members.members])
+            bot_room_exists = ms == set((BOT_ID, USER_ID))
+            if bot_room_exists:
+                self.bot_room_id = room
+                break
+        if not bot_room_exists:
+            resp = await self.room_create(is_direct=True, invite=[bot_user_id])
+            logging.info(resp)
+            resp = await self.room_invite(resp.room_id, bot_user_id)
+            logging.info(resp)
+        timestamp = str(datetime.now())
+        logging.info("User sending message to {}".format(self.bot_room_id))
+        await self.send_message(self.bot_room_id, "Hej at {}".format(timestamp))
+        await self.sync_and_close()
+
 async def register_dummy_users():
     client = AsyncClient(HOMESERVER_BASE_URL)
     resp = await client.register(BOT_ID, BOT_PW)
     resp = await client.register(USER_ID, USER_PW)
     await client.close()
 
-async def user_coroutine(bot_user_id):
-    user_client = UserClient(HOMESERVER_BASE_URL, USER_ID, USER_PW)
-    resp = await user_client.login()
-    logging.info(resp)
-    logging.info("Inviting {}".format(bot_user_id))
-    resp = await user_client.joined_rooms()
-    logging.info(resp)
-    joined_rooms = resp.rooms
-    bot_room_exists = False
-    for room in joined_rooms:
-        logging.info(room)
-        joined_members = await user_client.joined_members(room)
-        ms = set([m.display_name for m in joined_members.members])
-        bot_room_exists = ms == set((BOT_ID, USER_ID))
-        if bot_room_exists:
-            break
-    if not bot_room_exists:
-        resp = await user_client.room_create(is_direct=True, invite=[bot_user_id])
-        logging.info(resp)
-        resp = await user_client.room_invite(resp.room_id, bot_user_id)
-        logging.info(resp)
-    await user_client.sync_and_close()
-
 async def main():
     await register_dummy_users()
     bot_client = BotClient(HOMESERVER_BASE_URL, BOT_ID, BOT_PW)
     resp = await bot_client.login()
     logging.info(resp)
-    #bot_future = asyncio.run_coroutine_threadsafe(bot_client.sync_and_close(), asyncio.get_event_loop())
-    #user_future = asyncio.run_coroutine_threadsafe(user_coroutine(bot_client.user_id), asyncio.get_event_loop())
-    bot_task = asyncio.create_task(bot_client.sync_and_close())
-    user_task = asyncio.create_task(user_coroutine(bot_client.user_id))
+    user_client = UserClient(HOMESERVER_BASE_URL, USER_ID, USER_PW)
+    bot_task = asyncio.create_task(bot_client.main())
+    user_task = asyncio.create_task(user_client.main(bot_client.user_id))
     await asyncio.gather(
         bot_task,
         user_task
