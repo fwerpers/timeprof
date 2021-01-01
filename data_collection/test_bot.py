dde97381710046514a470b64dc374bcaad6df568
Felix Werpers <felix@werpers.com>
2021-01-01 12:52:20 +0100

    Make test result depend on an asyncio event status

--- data_collection/test_bot.py
+++ data_collection/test_bot.py
@@ -1,5 +1,6 @@
 from nio import (
     AsyncClient,
+    AsyncClientConfig,
     InviteEvent,
     RoomMessageText,
     SyncResponse
@@ -22,6 +23,7 @@ USER_PW = "user_pw"
 BOT_START_STR = "Hello from bot"
 MSG_TIME_LIMIT_MS = 5e3
 MS_PER_S = 1e3
+ASK_FOR_SAMPLE_MSG = ""
 
 class BotClient(AsyncClient):
     def __init__(self, homeserver, mid, mpw):
@@ -29,6 +31,7 @@ class BotClient(AsyncClient):
         self.mpw = mpw
         self.add_event_callback(self.autojoin_room, InviteEvent)
         self.add_event_callback(self.message_callback, RoomMessageText)
+        logging.info("BotClient initialised")
 
     def login(self):
         return super().login(self.mpw)
@@ -36,6 +39,7 @@ class BotClient(AsyncClient):
     async def message_callback(self, room, event):
         if event.sender != self.user_id:
             logging.info(event)
+            await self.send_message(room.room_id, "Response from BotClient")
         #time_now = int(round(time.time() * MS_PER_S))
         #if event.sender != self.user_id and time_now - event.server_timestamp < MSG_TIME_LIMIT_MS:
         #    logging.info(event)
@@ -82,9 +86,9 @@ class UserClient(AsyncClient):
         time_now = datetime.now()
         logging.info(time_now)
         logging.info(time_now - self.start_time)
-        if time_now - self.start_time > timedelta(seconds=10):
-            logging.info("Closing down")
-            raise asyncio.CancelledError
+        #if time_now - self.start_time > timedelta(seconds=10):
+            #logging.info("UserClient closing down")
+            #raise asyncio.CancelledError
 
     async def message_callback(self, room, event):
         time_now = int(round(time.time() * MS_PER_S))
@@ -110,9 +114,11 @@ class UserClient(AsyncClient):
         await self.sync_forever(timeout=SYNC_TIMEOUT)
         await self.close()
 
-    async def main(self, bot_user_id):
+    async def main(self, bot_user_id, event):
         resp = await self.login()
         logging.info(resp)
+        event.set()
+        raise CancelledError
         resp = await self.joined_rooms()
         #logging.info(resp)
         joined_rooms = resp.rooms
@@ -135,29 +141,42 @@ class UserClient(AsyncClient):
         await self.sync_and_close()
 
 async def register_dummy_users():
-    client = AsyncClient(HOMESERVER_BASE_URL)
+    client_config = AsyncClientConfig(max_timeouts=1)
+    client = AsyncClient(HOMESERVER_BASE_URL, config=client_config)
     resp = await client.register(BOT_ID, BOT_PW)
+    logging.info(resp)
     resp = await client.register(USER_ID, USER_PW)
+    logging.info(resp)
     await client.close()
 
-async def main():
+async def run_bots():
+    logging.info("Registering dummy users...")
     await register_dummy_users()
     bot_client = BotClient(HOMESERVER_BASE_URL, BOT_ID, BOT_PW)
     resp = await bot_client.login()
     logging.info(resp)
     user_client = UserClient(HOMESERVER_BASE_URL, USER_ID, USER_PW)
     bot_task = asyncio.create_task(bot_client.main())
-    user_task = asyncio.create_task(user_client.main(bot_client.user_id))
+    user_event = asyncio.Event()
+    user_task = asyncio.create_task(user_client.main(bot_client.user_id, user_event))
     #await asyncio.gather(
     #    bot_task,
     #    user_task,
     #    return_exceptions=True
     #)
-    await asyncio.wait(
+    done, pending = await asyncio.wait(
         {user_task,
         bot_task},
-        return_when=asyncio.FIRST_COMPLETED
+        return_when=asyncio.FIRST_COMPLETED,
+        timeout=10
     )
+    logging.info(done)
+    logging.info(type(done))
+    logging.info(pending)
+    for t in pending:
+        t.cancel()
+    return user_event.is_set()
+    #return user_task in done
 
 @pytest.fixture 
 def event_loop(): 
@@ -165,11 +184,24 @@ def event_loop():
     yield loop 
     loop.close()
 
+def event_loop2():
+    loop = asyncio.get_event_loop() 
+    yield loop 
+    loop.close()
+
 def test_create_new_bot_room(event_loop):
     logging.basicConfig(level=logging.INFO)
-    event_loop.run_until_complete(main())
-    assert True
+    done = event_loop.run_until_complete(run_bots())
+    logging.info(done)
+    assert done 
+    #assert False
+    #assert True 
+
+def main2():
+    loop = event_loop2()
+    next(loop).run_until_complete(run_bots())
 
 if __name__ == "__main__":
-   logging.basicConfig(level=logging.INFO)
-   asyncio.get_event_loop().run_until_complete(main())
+    logging.basicConfig(level=logging.INFO)
+    main2()
+    #asyncio.get_event_loop().run_until_complete(run_bots())
